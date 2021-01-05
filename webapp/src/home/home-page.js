48c67b72e92527f6e27144c436c539b073ee5417
Rachele Edwards <dot.com1472@gmail.com>
2021-01-05 10:58:03 -0700

    removed dead code - commented out Link

--- webapp/src/home/home-page.js
+++ webapp/src/home/home-page.js
@@ -3,7 +3,6 @@ import React, { Fragment } from 'react'
 export function Home () {
   return (
     <Fragment>
-      {/* <Link to='/another'>Another route</Link> */}
       <div>Ready, steady, go!</div>
     </Fragment>
   )
