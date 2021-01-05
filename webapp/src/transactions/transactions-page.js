4100ab056189efc1e0e6d2fc0a5ff2d4b4a5d1bf
Rachele Edwards <dot.com1472@gmail.com>
2021-01-05 10:52:00 -0700

    created transactions component reflecting Home to update route

---
+++ webapp/src/transactions/transactions-page.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+export function Transactions () {
+  return (
+    <Fragment>
+      {/* <Link to='/'>Home/Link> */}
+      <div>Content for /another route</div>
+    </Fragment>
+  )
+}
