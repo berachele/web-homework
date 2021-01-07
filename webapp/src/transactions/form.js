7a9e1d1d1b24f31a5009a9f1e9ddb624bfb94328
Rachele Edwards <dot.com1472@gmail.com>
2021-01-06 20:16:35 -0700

    moved Form to its own component

---
+++ webapp/src/transactions/form.js
@@ -0,0 +1,30 @@
+function Form(props){
+    return (
+      <form onSubmit={props.handleSubmit}>
+          <label> Amount &nbsp;
+            <input
+              onChange={props.handleChange} 
+              value={props.formValues.amount}
+              name='amount'
+              type='text'
+            />
+          </label>
+          <br/>
+          
+          <label> Date &nbsp;
+            <input
+              onChange={props.handleChange} 
+              value={props.formValues.date}
+              name='date'
+              type='text'
+            />
+          </label>
+          <br /><br />
+  
+          <input type='submit' />
+        </form>
+    )
+  }
+
+  export default Form
+  
