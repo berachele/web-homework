07b9dae51cf316e8e275da27ab3b2d6e8ae1e364
Rachele Edwards <dot.com1472@gmail.com>
2021-01-08 11:15:11 -0700

    created layout of tests to do and finsihed snapshot test

--- webapp/__tests__/Transactions.test.js
+++ webapp/__tests__/Transactions.test.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom'
+import { render, screen, cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer'
+
 import { Transactions } from '../src/transactions';
 
+afterEach(cleanup);
+
 describe('Transactions page test suite', () => {
-  it('renders "Content for /transactions route" message', () => {
+  it('renders "List of Transactions" message', () => {
     render(<Transactions />);
-    expect(screen.getByText('Content for /transactions route')).toBeInTheDocument();
+    expect(screen.getByText('List of Transactions')).toBeInTheDocument();
+  });
+
+  //this will also include Form snapshot because Form is imported in Transactions
+  it('matches Transaction component snapshot', () => {
+    const tree = renderer.create(<Transactions/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+  it('Form to enter and add transaction successfully works', () => {
+    render(<Transactions/>);
+    
+  });
+
+  it('Clicking Delete Icon successfully deletes that transaction', () => {
+
+  });
+
+  it('Clicking Edit Icon successfully open up edit form', () => {
+
+  });
+
+  it('Form to edit transaction successfully works', () => {
+
   });
 });
