9bee2b457699ac8cfd09ee29963885f11ffc4e18
Rachele Edwards <dot.com1472@gmail.com>
2021-01-12 15:36:02 -0700

    installed react minimal pie chart to get pie chart rendering

--- webapp/src/home/home-page.js
+++ webapp/src/home/home-page.js
@@ -1,9 +1,20 @@
 import React, { Fragment } from 'react'
+import { PieChart } from 'react-minimal-pie-chart';
+
 
 export function Home () {
   return (
     <Fragment>
       <div>Ready, steady, go!</div>
+      <PieChart
+        style={{width: '25%'}}
+        labelPosition={'10%'}
+        data={[
+          { title: 'One', value: 10, color: '#1B3FB0' },
+          { title: 'Two', value: 15, color: '#FEC73A' },
+          { title: 'Three', value: 20, color: '#1B915A' },
+        ]}
+      />
     </Fragment>
   )
 }
