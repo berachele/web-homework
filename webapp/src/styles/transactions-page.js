04c9652c35a83a47347c5c6b30f536c4561b210a
Rachele Edwards <dot.com1472@gmail.com>
2021-01-14 15:39:29 -0700

    starting styles in transactions - created boxed inputs like in dashboard

---
+++ webapp/src/styles/transactions-page.js
@@ -0,0 +1,14 @@
+import { css } from '@emotion/core'
+
+export const containerStyles = css`
+    border: 2px solid #9a8c98;
+    background: rgba(154, 140, 152, .6);
+    border-radius: 1%;
+    width: 50%;
+    padding: 1%;
+
+
+    & > h2 {
+        margin: 0;
+    }
+`
