a063ca97b28c2182a73886d8f9aa4a6c71778987
Rachele Edwards <dot.com1472@gmail.com>
2021-01-14 14:29:38 -0700

    added background color and credit info box on dashboard, moving over styles to emotion

--- webapp/src/home/home-page.js
+++ webapp/src/home/home-page.js
@@ -1,42 +1,45 @@
 import React, { Fragment } from 'react'
 import { PieChart } from 'react-minimal-pie-chart';
-
+import { creditInfoStyles, chartKeyStyles, greenKeyBox, blackKeyBox, pieChartStyles } from '../styles/home-page'
 
 export function Home () {
   return (
     <Fragment>
-      <h2>Credit Info</h2>
-      <div style={{display: 'flex', alignItems: 'center', alignContent: 'center'}}>
-        <div style={{width: '5%', height: '20px', background: '#169F6E', margin: '1%', borderRadius: '5%'}}></div>
-        <p>Available</p>
-        <div style={{width: '5%', height: '20px', background: 'black', margin: '0 1%', borderRadius: '5%'}}></div
-        ><p>Spent</p>
+      <h1>Dashboard</h1>
+      <div css={creditInfoStyles}>
+        <h2>Credit Info</h2>
+        <div css={chartKeyStyles}>
+          <div css={ greenKeyBox }></div>
+          <p>Available</p>
+          <div css={blackKeyBox}></div
+          ><p>Spent</p>
+        </div>
+        <PieChart
+          css={pieChartStyles}
+          data={[
+            {
+            color: "#169F6E",
+            title: "Available",
+            value: 30,
+            },
+            {
+            color: "black",
+            title: "Spent",
+            value: 15,
+            },
+          ]}
+          label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}%`}
+          labelPosition={80}
+          labelStyle={{ fontSize: '50%', fontWeight: 'bold'  }}
+          lengthAngle={360}
+          lineWidth={40} 
+          paddingAngle={40}
+          radius={50} 
+          rounded
+          startAngle={85}
+          viewBoxSize={[100, 100]}
+        />
       </div>
-      <PieChart
-        style={{ width: '25%' }}
-        data={[
-          {
-          color: "#169F6E",
-          title: "Available",
-          value: 30,
-          },
-          {
-          color: "black",
-          title: "Spent",
-          value: 15,
-          },
-        ]}
-        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}%`}
-        labelPosition={80}
-        labelStyle={{ fontSize: '50%', fontWeight: 'bold'  }}
-        lengthAngle={360}
-        lineWidth={40} 
-        paddingAngle={40}
-        radius={50} 
-        rounded
-        startAngle={85}
-        viewBoxSize={[100, 100]}
-      />
     </Fragment>
   )
 }
